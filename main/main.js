645255ef7c1231735d449f62c1db24eda3e8eb7d
rimuuu <soryonew@gmail.com>
2020-04-29 20:53:22 +0900

    클릭 시 댓글 추가 기능 완료/ 버튼 활성화

--- main/main.js
+++ main/main.js
@@ -29,17 +29,31 @@ for (let i = 0; i < comment.length; i++) {
   });
 }
 
-let btn = document.getElementsByTagName("button");
+let btn = document.querySelectorAll(".coBtn");
 
 for (let i = 0; i < btn.length; i++) {
-  let comment_text = comment[i].value;
+  comment[i].addEventListener("keydown", function () {
+    if (comment[i].value !== "") {
+      btn[i].classList.add("activeBtn");
+    } else {
+      btn[i].classList.remove("activeBtn");
+    }
+  });
+}
 
-  btn[i].addEventListener("keypress", function () {
-    console.log("hi");
-    // if (comment_text !== "") {
-    //   btn[i].classList.add("activeBtn");
-    // } else {
-    //   btn[i].classList.remove("activeBtn");
-    // }
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", function (e) {
+    let createComment = document.createElement("li");
+    let commentId = document.querySelector(".comment-id").innerHTML;
+
+    createComment;
+    createComment.className = "new";
+
+    let comment_text = comment[i].value;
+
+    createComment.innerHTML = `${commentId}   ${comment_text}`;
+
+    commentList[i].appendChild(createComment);
+    comment[i].value = "";
   });
 }

8e7dd931cb48843757a20246b5ef3618ded4fb1e
rimuuu <soryonew@gmail.com>
2020-04-29 17:57:15 +0900

    인풋 텍스트 입력 시 버튼 활성화 구현중

--- main/main.js
+++ main/main.js
@@ -1,5 +1,14 @@
 let comment = document.querySelectorAll(".no-outline"); //input
 let commentList = document.querySelectorAll(".comments-list");
+let form = document.querySelectorAll(".comments-form");
+
+function handleSubmit(e) {
+  e.preventDefault();
+}
+
+for (let i = 0; i < form.length; i++) {
+  form[i].addEventListener("submit", handleSubmit);
+}
 
 for (let i = 0; i < comment.length; i++) {
   comment[i].addEventListener("keypress", function (e) {
@@ -20,16 +29,17 @@ for (let i = 0; i < comment.length; i++) {
   });
 }
 
-const btn = document.querySelectorAll("button");
-for(let i =0; i < btn.length; i++ ){
-    btn[i].addEventListener('click', function(e){
-        if (comment.value !== "") {
-          classList.add("");
-        } else {
-          login_btn.classList.remove("");
-        }
-      })
+let btn = document.getElementsByTagName("button");
 
-  
+for (let i = 0; i < btn.length; i++) {
+  let comment_text = comment[i].value;
 
-
+  btn[i].addEventListener("keypress", function () {
+    console.log("hi");
+    // if (comment_text !== "") {
+    //   btn[i].classList.add("activeBtn");
+    // } else {
+    //   btn[i].classList.remove("activeBtn");
+    // }
+  });
+}

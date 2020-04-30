dea570dc62e83d20df749b48a58f24fee47bd6e3
rimuuu <soryonew@gmail.com>
2020-04-30 11:26:21 +0900

    메인페이지 레이아웃 수정 (flex 적용)

--- main/main.js
+++ main/main.js
@@ -10,6 +10,7 @@ for (let i = 0; i < form.length; i++) {
   form[i].addEventListener("submit", handleSubmit);
 }
 
+//엔터 시 댓글입력
 for (let i = 0; i < comment.length; i++) {
   comment[i].addEventListener("keypress", function (e) {
     if (e.key === "Enter") {
@@ -29,6 +30,7 @@ for (let i = 0; i < comment.length; i++) {
   });
 }
 
+//버튼 활성화
 let btn = document.querySelectorAll(".coBtn");
 
 for (let i = 0; i < btn.length; i++) {
@@ -41,6 +43,7 @@ for (let i = 0; i < btn.length; i++) {
   });
 }
 
+//클릭 시 버튼 입력
 for (let i = 0; i < btn.length; i++) {
   btn[i].addEventListener("click", function (e) {
     let createComment = document.createElement("li");
